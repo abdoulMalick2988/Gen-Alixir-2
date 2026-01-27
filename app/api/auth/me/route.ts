60b4dd94e3775af6d2ba25900a7148ee722b334b
abdoulMalick2988 <abdoulmalick2988@gmail.com>
2026-01-27 19:27:55 +0200

    Update route.ts

--- app/api/auth/me/route.ts
+++ app/api/auth/me/route.ts
@@ -5,6 +5,8 @@ import { NextRequest, NextResponse } from 'next/server';
 import prisma from '@/lib/prisma';
 import { extractToken, verifyToken } from '@/lib/auth';
 
+export const dynamic = 'force-dynamic';
+
 export async function GET(request: NextRequest) {
   try {
     // Extraire et vérifier le token
